214e0e655ed170ac9df1a6496dc443a8823c7c86
pemrogrmanwebkel9 <pemrogramanwebkel9@gmail.com>
2025-06-27 23:52:28 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const burger = document.getElementById("burger");
+  const navLinks = document.getElementById("navLinks");
+
+  burger.addEventListener("click", function () {
+    navLinks.classList.toggle("active");
+  });
+});
+
+// Efek muncul dari bawah saat discroll
+const menuItems = document.querySelectorAll('.menu-item');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+      // Hanya sekali tampil, lalu observer-nya dimatikan
+      observer.unobserve(entry.target);
+    }
+  });
+}, {
+  threshold: 0.2 // Saat 20% dari elemen terlihat, animasi mulai
+});
+
+menuItems.forEach(item => {
+  observer.observe(item);
+});
+
